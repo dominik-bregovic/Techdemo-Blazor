755d97146d58641bc8f4e078539df7e3291f3aac
Dominik Bregovic <dbregovic@tcm-international.com>
2022-10-06 16:49:53 +0200

    generated gitignore and exculded unnecessary project

--- BlazorAPI-All-In-One/Services/IEmployeeService.cs
+++
@@ -1,9 +0,0 @@
-﻿using BlazorAPI_All_In_One.Models;
-
-namespace BlazorAPI_All_In_One.Services
-{
-    public interface IEmployeeService
-    {
-        Task<List<Employee>> GetEmployees();
-    }
-}
--- MyModels/Models/Employee.cs
+++ MyModels/Models/Employee.cs
@@ -15,7 +15,6 @@ namespace MyModels.Models
         
         public int EmpId { get; set; }
         public string Name { get; set; }
-
         public int Index { get; set; }
     }
 }
